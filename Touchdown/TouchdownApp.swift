292d06626e7c29a9da596db16be89f657eb6710b
Denis <denis.yaremenko@huma.com>
2024-04-05 11:59:21 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Denis Yaremenko on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
